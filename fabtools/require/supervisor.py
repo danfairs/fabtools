e5b20a9ac27af1e4637495100095e229f89202f1
Dan Fairs <dan@fezconsulting.com>
2011-12-14 14:27:14 +0000

    Initial work on allowing a number of services to be added to supervisor, with the restart at the end.

--- fabtools/require/supervisor.py
+++ fabtools/require/supervisor.py
@@ -17,10 +17,7 @@ redirect_stderr=true
 """
 
 
-def process(name, template_contents=None, template_source=None, **kwargs):
-    """
-    Require a supervisor process
-    """
+def _process(name, template_contents=None, template_source=None, **kwargs):
     deb.package('supervisor')
 
     config_filename = '/etc/supervisor/conf.d/%s.conf' % name
@@ -32,9 +29,47 @@ def process(name, template_contents=None, template_source=None, **kwargs):
     if (template_contents is None) and (template_source is None):
         template_contents = DEFAULT_TEMPLATE
 
-    template_file(config_filename, template_contents, template_source, context, use_sudo=True)
+    template_file(config_filename, template_contents, template_source, context,
+        use_sudo=True)
+
+
+def process(name, template_contents=None, template_source=None, **kwargs):
+    """
+    Require a supervisor process
+    """
+    kw = {
+        'template_contents': template_contents,
+        'template_source': template_source,
+    }
+    kw.update(kwargs)
+    processes((name, kw),)
+
+
+def processes(process_infos):
+    """
+    Require a number of supervisor processes.
+
+      processes(
+        ('foo', {'template_source': 'bar'}),
+        ('quux', {'template_source': 'baz'}),
+    )
+
+    Supervisor takes a small amount of time to reload its configuration.
+    Calling process() repeatedly may therefore fail, if supervisor is still
+    reloading when subsequent calls are made. processes() allows a number of
+    processes to be configured with a single configuration reload.
+    """
+    names = []
+    for info in process_infos:
+        try:
+            name, kwargs = info
+        except ValueError:
+            name = info
+            kwargs = {}
+        _process(name, **kwargs)
+        names.append(name)
 
     reload_config()
-
-    if process_status(name) == 'STOPPED':
-        start_process(name)
+    for name in names:
+        if process_status(name) == 'STOPPED':
+            start_process(name)
